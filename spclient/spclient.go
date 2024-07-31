3d335b08aaabbc33d5c5b625488b757929a3650b
devgianlu <altomanigianluca@gmail.com>
2024-07-31 13:04:42 +0200

    chore: fix typo

--- spclient/spclient.go
+++ spclient/spclient.go
@@ -177,7 +177,7 @@ func (c *Spclient) ResolveStorageInteractive(fileId []byte, prefetch bool) (*sto
 	defer func() { _ = resp.Body.Close() }()
 
 	if resp.StatusCode == 503 {
-		log.Debugf("storage resolve returned service unavailable, retring...")
+		log.Debugf("storage resolve returned service unavailable, retrying...")
 		_ = resp.Body.Close()
 
 		resp, err = c.request("GET", path, nil, nil, nil)
