02918ae588d0d7ff2156c871de0f59f56aa55645
devgianlu <altomanigianluca@gmail.com>
2024-10-12 19:02:08 +0200

    fix: translate GID to URI when comparing tracks

--- tracks/utils.go
+++ tracks/utils.go
@@ -6,12 +6,17 @@ import (
 )
 
 func ContextTrackComparator(typ librespot.SpotifyIdType, target *connectpb.ContextTrack) func(*connectpb.ContextTrack) bool {
+	targetUri := target.Uri
+	if len(targetUri) == 0 && len(target.Gid) > 0 {
+		targetUri = librespot.SpotifyIdFromGid(typ, target.Gid).Uri()
+	}
+
 	return func(track *connectpb.ContextTrack) bool {
 		if len(track.Uid) > 0 && track.Uid == target.Uid {
 			return true
-		} else if len(track.Uri) > 0 && track.Uri == target.Uri {
+		} else if len(track.Uri) > 0 && track.Uri == targetUri {
 			return true
-		} else if len(track.Gid) > 0 && librespot.SpotifyIdFromGid(typ, track.Gid).Uri() == target.Uri {
+		} else if len(track.Gid) > 0 && librespot.SpotifyIdFromGid(typ, track.Gid).Uri() == targetUri {
 			return true
 		} else {
 			return false
