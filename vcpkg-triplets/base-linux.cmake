73f8fe6516d2114976e9c479841bf05b365c4d62
devgianlu <altomanigianluca@gmail.com>
2025-11-09 11:29:07 +0100

    chore: do not link alsa statically
    This causes compatibility issues. See #251

--- vcpkg-triplets/base-linux.cmake
+++ vcpkg-triplets/base-linux.cmake
@@ -7,3 +7,8 @@ set(VCPKG_BUILD_TYPE release)
 if(PORT STREQUAL "libflac")
     set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DBUILD_CXXLIBS=OFF)
 endif()
+
+if(PORT STREQUAL "alsa")
+    set(VCPKG_CRT_LINKAGE dynamic)
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
